c0b92e09074608ef03e9c0fe0be368f60f991e5b
Andrey Ivanov <prioritet2503@gmail.com>
2022-10-02 14:29:55 +0300

    save

--- db/contacts.js
+++ db/contacts.js
@@ -1,35 +1,39 @@
 const fs = require("fs").promises;
 const path = require("path");
+const { nanoid } = require("nanoid");
 
-/*
- * Раскомментируй и запиши значение
- * const contactsPath = ;
- */
-const contactsPath = path.resolve("contacts.json");
+const contactsPath = path.join(__dirname, "contacts.json");
 
-// TODO: задокументировать каждую функцию
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
-  const contacts = JSON.parse(data);
-  console.log(contacts);
-  return contacts;
+  return JSON.parse(data);
 };
 
-function getContactById(contactId) {
-  // ...твой код
-}
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((el) => el.id === contactId);
+  return result || null;
+};
 
-function removeContact(contactId) {
-  // ...твой код
-}
+// const removeContact = async (contactId) => {
+//   const contact = await listContacts();
+// };
 
-function addContact(name, email, phone) {
-  // ...твой код
-}
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
 
 module.exports = {
   listContacts,
   getContactById,
-  removeContact,
   addContact,
 };
--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-const { listContacts } = require("./db/contacts");
-console.log(listContacts);
+const contacts = require("./db/contacts");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "listContacts":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+    case "getContactById":
+      const getContact = await contacts.getContactById(id);
+      console.log(getContact);
+      break;
+    case "addContact":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+    case "updateContact":
+      const rewriteContact = await contacts.addContact(name, email, phone);
+      console.log(rewriteContact);
+      break;
+    default:
+      console.log("Unknown action");
+  }
+};
+// invokeAction({ action: "listContacts" });
+// invokeAction({ action: "getContactById", id: "3" });
+invokeAction({
+  action: "addContact",
+  name: "Olaf Bodruy",
+  email: "Bodruy.Olaf@nonenimMauris.net",
+  phone: "(777) 451-7038",
+});
